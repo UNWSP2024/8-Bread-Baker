a15787b6cc4f5c9525d2c2ab4232cdd51837c921
Bread-Baker <aefafach@students.unwsp.edu>
2026-03-19 13:46:42 -0500

    Wrote Program #2: Word Separator

--- program_2.py
+++ program_2.py
@@ -1,3 +1,8 @@
+# Name: Ariana Fafach
+# Date: 3/19/2026
+# Title: Program #2: Word Separator
+
+
 # Program #2: Word Separator
 # Write a program that accepts as input a sentence in which all of the words are run together, 
 # but the first character of each word is uppercase.  
@@ -7,16 +12,36 @@
 
 # Start your changes on line 13
 
+
 def word_separator(sentence):
 
-    new_sentence = ""
+    # Starts new_sentence with the first letter of the user's sentence without changing it.
+    new_sentence = sentence[0]
     #    Add your logic here
 
+    # For loop iterates over each letter of the user's sentence, skipping the first one.
+    for i in range (1, len(sentence)):
+
+        # Test if the letter is uppercase, and if it is, change it to lower case and add a space before it. Add it onto new_sentence.
+        if sentence[i].isupper() == True:
+            new_sentence += ' ' + sentence[i].lower()
+        
+        # If the letter is not uppercase, add it to new_sentence without changing it.
+        else:
+            new_sentence += sentence[i]
+    
+    # Test if the sentence already has punctuation. If not, add a period.
+    if sentence[-1] != '.' and sentence[-1] != '!' and sentence[-1] != '?':
+        new_sentence += '.'
+    
+    # Return new_sentence and strip all leading and trailing whitespaces.
     return new_sentence.strip()
 
 # Example usage
 if __name__=="__main__":
-    sentence = "StopAndSmellTheRoses"
+
+    # Get user input:
+    sentence = input("Enter a sentence in which all of the words run together, but the first letter of each word is uppercase:  ")
 
     new_sentence = word_separator(sentence)
 
